6e83196443cd369072d272269f36d25e6d896615
vladkornilov <bloodik39rus@gmail.com>
2020-07-01 21:45:37 +0300

    add chat

--- ware_src/index.js
+++ ware_src/index.js
@@ -4,6 +4,7 @@ import './images/feature1.png'
 import './images/feature2.png'
 import './images/feature3.png'
 import './images/feature4.png'
+import './images/admin.png'
 
 import './css/warehouse.css'
 import './css/warehouse-header.css'
@@ -16,3 +17,45 @@ import './css/warehouse-footer.css'
 
 import '@fortawesome/fontawesome-free/js/all'
 import "@fortawesome/fontawesome-free/css/all.min.css"
+
+
+function createMessageBlock(direction, text) {
+    var message = document.createElement("div");
+    message.className = "message " + direction;
+
+    var avatar = document.createElement("img");
+    avatar.className = "avatar";
+    avatar.src = 'images/admin.png';
+
+    var data = document.createElement("div");
+    data.className = "data";
+
+    var body = document.createElement("div");
+    body.className = "body";
+    body.append(text);
+    data.appendChild(body);
+    
+    message.appendChild(avatar);
+    message.appendChild(data);
+
+    return message;
+}
+
+document.getElementById('chat').onclick = function() {
+    if (document.getElementById("openchat").hidden) {
+        document.getElementById("openchat").hidden = false;
+    }
+    else document.getElementById("openchat").hidden = true;
+    // document.getElementById("chatcontent").text += "press";
+}
+
+document.getElementById('chatbutton').onclick = function() {
+    var text = document.getElementById('chatwrite').value;
+    document.getElementById('chatcontent').appendChild(createMessageBlock("left", text));
+
+    setTimeout(function() {
+        var txt = "Ваш звонок очень важен для нас!" +
+        " Ближайший оператор освободится через inf секунд.";
+        document.getElementById('chatcontent').appendChild(createMessageBlock("right", txt));
+    }, 1000);
+}
